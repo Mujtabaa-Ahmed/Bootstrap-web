5081a04bfc6e185ecae686bf62967816f5fc997e
Mujtabaa-Ahmed <mujtaba56383@gmail.com>
2023-09-27 11:38:35 +0500

    uploaded 1st project

---
+++ index.js
@@ -0,0 +1,12 @@
+// function myfunc() {
+//     alert("")
+// }
+
+
+
+// var a = document.querySelector(".val");
+// if (a == ' ') {
+//     alert('scam')
+// } else {
+//     alert('not scamed')
+// }
